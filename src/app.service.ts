5c908aa406fbb2030761a6d95eec11a955071246
Gowtham-19 <gowthamkumar1611@gmail.com>
2022-12-08 22:34:53 +0530

    code changes

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from "@nestjs/axios";
+import { map, catchError } from 'rxjs';
 
 @Injectable()
 export class AppService {
+  constructor(private http: HttpService){}
   getHello(): string {
     return 'Hello World!';
   }
+
+  async transferData(body){
+    return await this.http.post("http://localhost:3002",body,{
+      headers:{
+        "Content-Type":"application/json"
+      }
+    }).pipe(
+      map((res) => {
+     return res.data
+      }),
+    )
+  }
+
 }
